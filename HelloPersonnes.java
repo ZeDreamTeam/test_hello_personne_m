8254ea7ac087b1bdc044e17e59cda00f511203be
Mickael <micky0304@gmail.com>
2014-11-06 09:56:55 +0100

    Typo fix

--- HelloPersonnes.java
+++ HelloPersonnes.java
@@ -6,9 +6,9 @@ public class HelloPersonnes {
         Enseignant en = new Enseignant("Jones", "Indiana", 99, 0);
         Etudiant et = new Etudiant("Cartman", "Eric", 9, 3);
 
-        System.out.println("Personne " + p.getNom() + " " + p.getPrenom() + " - " + p.getAge() + " years old");
-        System.out.println("Enseignant " + en.getNom() + " " + en.getPrenom() + " - " + en.getAge() + " years old " + " - " + en.getHours() + " per week");
-        System.out.println("Etudiant " + et.getNom() + " " + et.getPrenom() + " - " + et.getAge() + " years old " + " -" + et.getNote() + "/10");
+        System.out.println("Personne: " + p.getNom() + " " + p.getPrenom() + " - " + p.getAge() + " years old");
+        System.out.println("Enseignant: " + en.getNom() + " " + en.getPrenom() + " - " + en.getAge() + " years old" + " - " + en.getHours() + " per week");
+        System.out.println("Etudiant: " + et.getNom() + " " + et.getPrenom() + " - " + et.getAge() + " years old" + " - " + et.getNote() + "/10");
 
 
     }
