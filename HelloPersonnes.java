fccc8dada5c1633f57e60490c0df373e3e96e3e7
Mickael <micky0304@gmail.com>
2014-11-07 08:49:53 +0100

    Testing EnseignantChercheur

--- HelloPersonnes.java
+++ HelloPersonnes.java
@@ -1,13 +1,13 @@
 import perso.*;
+import stuff.Publication;
 
 public class HelloPersonnes {
     public static void main(String[] args) {
-        Personne p[] = new Personne[3];
-        p[0] = new Personne("Lannister", "Jamie", 24);
-        p[1] = new Enseignant("Jones", "Indiana", 99, 0);
-        p[2] = new Etudiant("Cartman", "Eric", 9, 3);
-        for(Personne personne : p) {
-            System.out.println(personne.toString());
-        }
+        IChercheur chercheur = new EnseignantChercheur("tom", "Sawyer", 35,192);
+        chercheur.ajouterPublication(new Publication("La conception objet",2003));
+        chercheur.ajouterPublication(new Publication("La programmation objet",2004));
+
+        // printing the object attributes
+        System.out.println(chercheur);
     }
 }
